16fed65d3612968a83d4e9bed58e2b0f070b1380
Fareed <Fareed@DESKTOP-AQP70AK>
2022-12-22 10:25:27 +0300

    Add project files.

---
+++ ExamResultSystem/FrmMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ExamResultSystem
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnShowResult = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtPoint = new System.Windows.Forms.NumericUpDown();
+            this.LblGrade = new System.Windows.Forms.Label();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.LblHappy = new System.Windows.Forms.Label();
+            this.LblSad = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtPoint)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sınav Sonucu Hesapla";
+            // 
+            // BtnShowResult
+            // 
+            this.BtnShowResult.BackColor = System.Drawing.Color.CadetBlue;
+            this.BtnShowResult.FlatAppearance.BorderSize = 0;
+            this.BtnShowResult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnShowResult.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnShowResult.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnShowResult.Location = new System.Drawing.Point(210, 77);
+            this.BtnShowResult.Name = "BtnShowResult";
+            this.BtnShowResult.Size = new System.Drawing.Size(115, 27);
+            this.BtnShowResult.TabIndex = 2;
+            this.BtnShowResult.Text = "Sonuç &Göster";
+            this.BtnShowResult.UseVisualStyleBackColor = false;
+            this.BtnShowResult.Click += new System.EventHandler(this.BtnShowResult_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sınav Puanı";
+            // 
+            // TxtPoint
+            // 
+            this.TxtPoint.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPoint.Location = new System.Drawing.Point(16, 77);
+            this.TxtPoint.Name = "TxtPoint";
+            this.TxtPoint.Size = new System.Drawing.Size(188, 26);
+            this.TxtPoint.TabIndex = 4;
+            // 
+            // LblGrade
+            // 
+            this.LblGrade.AutoSize = true;
+            this.LblGrade.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGrade.Location = new System.Drawing.Point(15, 127);
+            this.LblGrade.Name = "LblGrade";
+            this.LblGrade.Size = new System.Drawing.Size(37, 41);
+            this.LblGrade.TabIndex = 5;
+            this.LblGrade.Text = "_";
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResult.Location = new System.Drawing.Point(67, 135);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(89, 26);
+            this.LblResult.TabIndex = 6;
+            this.LblResult.Text = "_______";
+            // 
+            // LblHappy
+            // 
+            this.LblHappy.AutoSize = true;
+            this.LblHappy.Font = new System.Drawing.Font("Franklin Gothic Medium", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblHappy.ForeColor = System.Drawing.Color.Green;
+            this.LblHappy.Location = new System.Drawing.Point(133, 122);
+            this.LblHappy.Name = "LblHappy";
+            this.LblHappy.Size = new System.Drawing.Size(56, 43);
+            this.LblHappy.TabIndex = 7;
+            this.LblHappy.Text = "☻";
+            this.LblHappy.Visible = false;
+            // 
+            // LblSad
+            // 
+            this.LblSad.AutoSize = true;
+            this.LblSad.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSad.ForeColor = System.Drawing.Color.OrangeRed;
+            this.LblSad.Location = new System.Drawing.Point(175, 129);
+            this.LblSad.Name = "LblSad";
+            this.LblSad.Size = new System.Drawing.Size(54, 37);
+            this.LblSad.TabIndex = 8;
+            this.LblSad.Text = "☹";
+            this.LblSad.Visible = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 188);
+            this.Controls.Add(this.LblSad);
+            this.Controls.Add(this.LblHappy);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.LblGrade);
+            this.Controls.Add(this.TxtPoint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnShowResult);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Sınav Sonuçları";
+            ((System.ComponentModel.ISupportInitialize)(this.TxtPoint)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnShowResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown TxtPoint;
+        private System.Windows.Forms.Label LblGrade;
+        private System.Windows.Forms.Label LblResult;
+        private System.Windows.Forms.Label LblHappy;
+        private System.Windows.Forms.Label LblSad;
+    }
+}
+
---
+++ ExamResultSystem/FrmMain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamResultSystem
+{
+    public partial class FrmMain : Form
+    {
+        enum TLetterGrade {LgNull, LgA, LgB, LgC, LgD, LgE, LgF };
+        enum TExamResult {ErNull, ErPass, ErFail, ErSecondChance };
+        TLetterGrade LetterGrade;
+        TExamResult ExamResult;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShowResult_Click(object sender, EventArgs e)
+        {
+            LblHappy.Visible = false;
+            LblSad.Visible = false;
+            int Point = (int)TxtPoint.Value;
+            int Result = ShowResult(Point, out LetterGrade);
+            if (Result == 2)
+            {
+                LblResult.Text = "(Geçti)";
+                LblHappy.Visible = true;
+            }
+            else if (Result == 1)
+                LblResult.Text = "(Büte Kaldı)";
+            else if (Result == 0)
+            {
+                LblResult.Text = "(Kaldı)";
+                LblSad.Visible = true;
+            }
+            switch (LetterGrade)
+            {
+                case TLetterGrade.LgNull:
+                    break;
+                case TLetterGrade.LgA:
+                    LblGrade.Text = "A";
+                    break;
+                case TLetterGrade.LgB:
+                    LblGrade.Text = "B";
+                    break;
+                case TLetterGrade.LgC:
+                    LblGrade.Text = "C";
+                    break;
+                case TLetterGrade.LgD:
+                    LblGrade.Text = "D";
+                    break;
+                case TLetterGrade.LgE:
+                    LblGrade.Text = "E";
+                    break;
+                case TLetterGrade.LgF:
+                    LblGrade.Text = "F";
+                    break;
+                default:
+                    break;
+            }
+        }
+        int ShowResult(int Point, out TLetterGrade LeterGrade)
+        {
+            int Result = 0;
+            LeterGrade = TLetterGrade.LgNull;
+            if (Point >= 85)
+            {
+                LetterGrade = TLetterGrade.LgA;
+                Result= 2;
+            }
+            else if (Point >= 75 && Point < 85)
+            {
+                LetterGrade = TLetterGrade.LgB;
+                Result = 2;
+            }
+            else if (Point >= 65 && Point < 75)
+            {
+                LetterGrade = TLetterGrade.LgC;
+                Result= 2;
+            }
+            else if (Point >= 55 && Point < 65)
+            {
+                LetterGrade = TLetterGrade.LgD;
+                Result= 1;
+            }
+            else if (Point >= 45 && Point < 55)
+            {
+                LetterGrade = TLetterGrade.LgE;
+                Result= 1;
+            }
+            else if (Point<45)
+            {
+                LetterGrade= TLetterGrade.LgF;
+                Result= 0;
+            }
+            return Result;
+        }
+    }
+}
